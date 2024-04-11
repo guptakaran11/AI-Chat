3bb870a35d2b7b85189fec9ef13bf04b210933b9
guptakaran11 <gupta.karan1162@gmail.com>
2024-04-11 19:44:08 +0530

    chat provider is initialized

--- lib/Controller/provider/chatProvider.dart
+++ lib/Controller/provider/chatProvider.dart
@@ -1,18 +1,46 @@
 // ignore_for_file: file_names
 
 //* Packages
+
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart' as path;
 
+//* Models
+import 'package:aichat/Model/userModel.dart';
+
+//* Services
+import 'package:aichat/Controller/Services/HiveStorage/chatHistoryStorage.dart';
+import 'package:aichat/Controller/Services/HiveStorage/setting.dart';
+
 //* Widgets
 import 'package:aichat/View/Widgets/constants.dart';
 
-
 class ChatProvider extends ChangeNotifier {
+  // init Hive Box
   static initHive() async {
     final dir = await path.getApplicationDocumentsDirectory();
     Hive.init(dir.path);
-    await Hive.initFlutter(Constants.geminiDB); 
+    await Hive.initFlutter(Constants.geminiDB);
+
+    // register the adapters
+    if (!Hive.isAdapterRegistered(0)) {
+      Hive.registerAdapter(
+        ChatHistoryStorageAdapter(),
+      );
+      await Hive.openBox<ChatHistoryStorage>(Constants.chatHistoryBox);
+    }
+    if (!Hive.isAdapterRegistered(1)) {
+      Hive.registerAdapter(
+        UserModelAdapter(),
+      );
+      await Hive.openBox<UserModel>(Constants.userBox);
+    }
+    if (!Hive.isAdapterRegistered(2)) {
+      Hive.registerAdapter(
+        SettingsAdapter(),
+      );
+      await Hive.openBox<Settings>(Constants.settingBox);
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
-
-//* Packages 
+//* Packages
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+//* Providers
+import 'package:aichat/Controller/provider/chatProvider.dart';
 
 //* Screens
 import 'package:aichat/View/screens/homePage.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ChatProvider.initHive();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ChatProvider>(
+          create: (_) => ChatProvider(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,13 +30,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen()
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'AI Chat',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen());
   }
 }
