145cac063ba75e6b37dfb002f13cad021ce9d046
guptakaran11 <gupta.karan1162@gmail.com>
2024-04-14 19:51:11 +0530

    chat Provider backend functions done

--- lib/Controller/provider/chatProvider.dart
+++ lib/Controller/provider/chatProvider.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: file_names
 
-//* Packages
+//* Dart Packages
+import 'dart:async';
+import 'dart:developer';
 
+//* Packages
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -16,6 +19,7 @@ import '../../Model/messageModel.dart';
 //* Services
 import '../../Controller/Services/HiveStorage/chatHistoryStorage.dart';
 import '../../Controller/Services/HiveStorage/setting.dart';
+import '../../Controller/Services/Apis/api.dart';
 
 //* Widgets
 import '../../View/Widgets/constants.dart';
@@ -38,7 +42,7 @@ class ChatProvider extends ChangeNotifier {
 
   List<MessageModel> get inChatMessage => inChatMessages;
   PageController get pageController => controller;
-  List<XFile>? get imageFileLists => imagesFileList;
+  List<XFile>? get imagesFileLists => imagesFileList;
   int get currentIndexs => currentIndex;
   String get currentChatIds => currentChatId;
   GenerativeModel? get gmodel => model;
@@ -46,6 +50,77 @@ class ChatProvider extends ChangeNotifier {
   GenerativeModel? get visionModels => visionModel;
   String get gModelType => modelType;
   bool get isLoadings => isLoading;
+
+  Future<void> setInChatMessages(String chatId) async {
+    final messageFromDB = await loadMessagesFromDB(chatId);
+
+    for (var message in messageFromDB) {
+      if (inChatMessages.contains(message)) {
+        log("Message already exists");
+        continue;
+      }
+      inChatMessages.add(message);
+    }
+    notifyListeners();
+  }
+
+  Future<List<MessageModel>> loadMessagesFromDB(String chatId) async {
+    await Hive.openBox('${Constants.chatMessageBox}$chatId');
+
+    final messageBox = Hive.box('${Constants.chatMessageBox}$chatId');
+
+    final newData = messageBox.keys.map((e) {
+      final message = messageBox.get(e);
+      final messageData =
+          MessageModel.fromMap(Map<String, dynamic>.from(message));
+      return messageData;
+    }).toList();
+    notifyListeners();
+    return newData;
+  }
+
+  void setImageFileList(List<XFile> listValue) {
+    imagesFileList = listValue;
+    notifyListeners();
+  }
+
+  String setCurrentModel(String newModel) {
+    modelType = newModel;
+    notifyListeners();
+    return modelType;
+  }
+
+  Future<void> setModel(bool isTextOnly) async {
+    if (isTextOnly) {
+      model = textModel ??
+          GenerativeModel(
+            model: setCurrentModel('gemini-pro'),
+            apiKey: ApiServices.apiKey,
+          );
+    } else {
+      model = visionModel ??
+          GenerativeModel(
+            model: setCurrentModel('gemini-pro-vision'),
+            apiKey: ApiServices.apiKey,
+          );
+    }
+    notifyListeners();
+  }
+
+  void setCurrentIndex(int newIndex) {
+    currentIndex = newIndex;
+    notifyListeners();
+  }
+
+  void setCurrentChatID(String newChatId) {
+    currentChatId = newChatId;
+    notifyListeners();
+  }
+
+  void setLoading(bool value) {
+    isLoading = value;
+    notifyListeners();
+  }
 
   // init Hive Box
   static initHive() async {
--- lib/View/Widgets/constants.dart
+++ lib/View/Widgets/constants.dart
@@ -3,5 +3,7 @@ class Constants {
   static const String userBox = "userBox";
   static const String settingBox = "settings";
 
+  static const String chatMessageBox = "chat_messages_";
+
   static const String geminiDB = 'gemini.db';
 }
